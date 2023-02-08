b96faa07e786e626b555a4dcc211d25d48021c94
Lev <fedorichev16@gmail.com>
2023-02-08 20:26:50 +0300

    update scripts

--- create.sql
+++ create.sql
@@ -1,10 +1,10 @@
 
 CREATE TABLE "PERSONS"
 (
-    name varchar(255),
-    surname varchar(255),
-    age integer NOT NULL,
-    phone_number varchar(50),
+	name           varchar(255) not null,
+    surname        varchar(255) not null,
+    age            int check (age > 0),
+    phone_number   varchar(255),
     city_of_living varchar(255),
-    CONSTRAINT "PERSONS_pkey" PRIMARY KEY (name, surname, age)
+    primary key (name, surname, age)
 )
